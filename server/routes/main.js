835adc41f6013587eac3f2ee7a8f6628575a9265
mariayoivanova <maria.yo.ivanova@gmail.com>
2024-06-13 09:41:19 +0200

    inserting articles from db in the site

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,17 +1,62 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../models/Post');
 
+/**
+ * GET /
+ * HOME
+ */
 
 //Routes
-router.get('', (req, res) => {
+router.get('', async (req, res) => {
 
-    const locals = {
-        titel: "Blog",
-        description: "Blog usefull for everyone"
+   
+
+    try {
+        const locals = {
+            titel: "Blog",
+            description: "Blog usefull for everyone"
+        }
+        
+        
+        let perPage = 6;
+        let page = req.query.page || 1;
+
+        const data = await Post.aggregate([ {$sort: { createdAd: -1 } } ])
+        .skip(perPage * page - perPage)
+        .limit(perPage)
+        .exec();
+
+        const count = await Post.count();
+        const nextPage = parseInt(page) + 1;
+        const hasNextPage = nextPage <=Math.ceil(count / perPage);
+
+
+        res.render('index', {
+            locals, 
+            data,
+            current: page,
+            nextPage: hasNextPage ? nextPage : null
+
+        });
+    } catch (error){
+        console.log(error);
     }
-
-    res.render('index', {locals});
+  
 });
+
+
+/*
+function insertPostData(){
+    Post.insertMany([
+        {
+            titel: "Building a blog",
+            body: "This is the body text"
+        }
+    ])
+}
+*/
+//insertPostData();
 
 router.get('/about', (req, res) => {
     res.render('about');
